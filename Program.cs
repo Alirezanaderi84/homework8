0d8f80cec7827185a82a7be4706890051471ab97
Alirezanaderi84 <148426222+Alirezanaderi84@users.noreply.github.com>
2024-01-01 09:47:01 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PLZ ENTER A NUM>0");
+            int x = int.Parse(Console.ReadLine());
+            int y = fib(x);
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+        static int fib(int x)
+        {
+            if (x <= 2)
+                return x - 1;
+            else
+                return fib(x - 1) + fib(x - 2);
+        }
+    }
+    
+}
